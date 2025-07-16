c7518739fddf7aec2d5819370cd439cf62b74f83
djinylh <d.jinylh@gmail.com>
2025-07-16 16:12:36 +0900

    "component, view 파일 생성 / sass, 부트스트랩 설치"

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="navbar shadow-sm">
+      <div class="container">
+        <router-link to="/" class="navbar-brand"> </router-link>
+        <div class="menus d-flex gap-3">
+          <a>로그인</a>
+          <a>주문 내역</a>
+          <a>장바구니</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  background-color: #ff6f3c;
+  color: aliceblue;
+}
+.container-xxl {
+  left: 200px !important;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomeSwiper from '@/components/HomeSwiper.vue';
+</script>
+
+<template>
+  <HomeSwiper></HomeSwiper>
+  <div>home</div>
+</template>
+
+<style scoped></style>
