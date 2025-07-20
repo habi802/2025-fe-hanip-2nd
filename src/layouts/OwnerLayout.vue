64ec1bb83d813c7d6b939ae1cccfe390914b24d8
root <nususu1004@gmail.com>
2025-07-21 01:28:45 +0900

    가게 수정 페이지

---
+++ src/layouts/OwnerLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <OwnerHeader />
+    <router-view />
+    <OwnerFooter />
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/Home.vue'),
+      path: "/",
+      name: "home",
+      component: () => import("@/views/Home.vue"),
     },
     {
-      path: '/login',
-      component: () => import('@/views/Login.vue')
+      path: "/login",
+      component: () => import("@/views/Login.vue"),
     },
     {
-      path: '/join',
-      component: () => import('@/views/Join.vue')
+      path: "/join",
+      component: () => import("@/views/Join.vue"),
     },
     {
-      path: '/categoryList',
-      component: () => import('@/views/Category.vue'),
+      path: "/categoryList",
+      component: () => import("@/views/Category.vue"),
     },
   ],
 });
