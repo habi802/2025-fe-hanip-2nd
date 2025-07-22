6537213486ebac3dc0f58d557956e69fa082cf6b
habi802 <shb8002@gmail.com>
2025-07-22 16:26:38 +0900

    가게 조회 화면 작업 중...

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -3,6 +3,8 @@ const props = defineProps({
     item: {
         name: String,
         rating: Number,
+        menuName: String,
+        menuCount: Number,
         comment: String,
         imagePath: String,
         ownerComment: String,
@@ -13,12 +15,36 @@ const props = defineProps({
 
 <template>
     <div class="border rounded p-3 mb-2">
-        <div class="row">
-            
+        <div class="mb-2">
+            <span class="pe-2">{{ props.item.name }}</span>
+            <span>{{ props.item.created }}</span>
+        </div>
+        <div class="mb-2">
+            <span>★★★★★</span>
+        </div>
+        <div class="p-2">
+            <div class="review-image border w-100"></div>
+        </div>
+        <div class="mb-2">
+            <span>{{ props.item.menuName + (props.item.menuCount > 1 ? (' 외 ' + (props.item.menuCount - 1)) + '개' : '') }}</span>
+        </div>
+        <div class="mb-2">
+            <span>{{ props.item.comment }}</span>
+        </div>
+        <div v-if="props.item.ownerComment !== null && props.item.ownerComment !== ''">
+            <div class="owner-comment border rounded p-2">
+                {{ props.item.ownerComment }}
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.owner-comment {
+    background-color: #797979;
+}
 
+.review-image {
+    height: 300px;
+}
 </style>
