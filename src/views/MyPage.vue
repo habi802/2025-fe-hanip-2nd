d6cb1d3c97220c04280e991a3254ea7ee42dbfdc
djinylh <d.jinylh@gmail.com>
2025-07-23 11:26:13 +0900

    진짜 풀 요청

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,134 +1,94 @@
-<script setup>
-import { reactive } from 'vue';
-const user = reactive({
-  name: '서하빈',
-  orders: 0,
-  cupon: 0,
-  point: 0,
-});
-const myAccount = [
-  '주문내역조회',
-  '정보수정',
-  '결제관리',
-  '즐겨찾기',
-  '고객센터',
-];
-</script>
-
 <template>
-  <div class="box">
-    <!-- 보더 왼쪽 -->
-    <div class="userboard">
-      <div class="userLeft">
-        <img class="userImg" src="/src/imgs/userImg.png" />
-        <div class="user">
-          <div>{{ user.name }}님 &nbsp; 반가워요!</div>
-          <div class="smalluser">
-            <span class="ip">한입</span>
-            <span class="rank">등급</span>
-          </div>
-        </div>
-      </div>
-      <!-- 보더 오른쪽 -->
-      <div class="userright">
-        <div class="right">
-          <div>총 주문 수</div>
-          <div class="num">{{ user.orders }}</div>
-        </div>
-        <div class="right">
-          <div>쿠폰</div>
-          <div class="num">{{ user.cupon }}</div>
-        </div>
-        <div class="right">
-          <div>포인트</div>
-          <div class="num">{{ user.point }}</div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div class="max-w-4xl mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-6">마이페이지</h1>
 
-  <div>
-    <div class="accountBoard">
-      <div>마이 페이지</div>
-      <div>{{ myAccount[0] }}</div>
-      <div>{{ myAccount[1] }}</div>
-      <div>{{ myAccount[2] }}</div>
-      <div>{{ myAccount[3] }}</div>
-      <div>{{ myAccount[4] }}</div>
+    <!-- 프로필 -->
+    <div class="bg-white p-4 rounded-xl shadow mb-6 flex items-center gap-4">
+      <img
+        src="/imgs/profile.png"
+        alt="프로필"
+        class="w-16 h-16 rounded-full border"
+      />
+      <div>
+        <p class="text-lg font-semibold">{{ user.name }} 님</p>
+        <p class="text-gray-500 text-sm">{{ user.email }}</p>
+      </div>
+      <button
+        @click="editProfile"
+        class="ml-auto bg-blue-500 text-white px-4 py-2 rounded-xl"
+      >
+        프로필 수정
+      </button>
+    </div>
+
+    <!-- 탭 메뉴 -->
+    <div class="flex gap-4 border-b mb-6">
+      <button
+        v-for="tab in tabs"
+        :key="tab"
+        @click="activeTab = tab"
+        :class="[
+          'px-4 py-2 font-semibold',
+          activeTab === tab
+            ? 'border-b-4 border-blue-500 text-blue-600'
+            : 'text-gray-500',
+        ]"
+      >
+        {{ tab }}
+      </button>
+    </div>
+
+    <!-- 컨텐츠 -->
+    <div>
+      <div v-if="activeTab === '주문 내역'">
+        <div
+          v-for="order in orders"
+          :key="order.id"
+          class="p-4 bg-white rounded-xl shadow mb-4"
+        >
+          <p class="font-bold">{{ order.storeName }}</p>
+          <p class="text-gray-500 text-sm">{{ order.date }}</p>
+          <p class="mt-2">{{ order.items }}</p>
+          <p class="font-semibold">{{ order.totalPrice }}원</p>
+        </div>
+      </div>
+
+      <div v-if="activeTab === '리뷰 관리'">
+        <div
+          v-for="review in reviews"
+          :key="review.id"
+          class="p-4 bg-white rounded-xl shadow mb-4"
+        >
+          <p class="font-bold">{{ review.storeName }}</p>
+          <p class="text-gray-500">{{ review.content }}</p>
+          <button class="text-blue-500 mr-2" @click="editReview(review.id)">
+            수정
+          </button>
+          <button class="text-red-500" @click="deleteReview(review.id)">
+            삭제
+          </button>
+        </div>
+      </div>
+
+      <div v-if="activeTab === '찜 목록'" class="grid grid-cols-2 gap-4">
+        <div
+          v-for="store in wishlist"
+          :key="store.id"
+          class="p-4 bg-white rounded-xl shadow"
+        >
+          <img
+            :src="store.image"
+            class="w-full h-32 object-cover rounded-xl mb-2"
+          />
+          <p class="font-semibold">{{ store.name }}</p>
+          <button class="mt-2 w-full bg-red-500 text-white py-1 rounded-lg">
+            찜 해제
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<script setup></script>
 
-<style lang="scss" scoped>
-@font-face {
-  font-family: 'BMJUA';
-  src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff')
-    format('woff');
-  font-weight: 600;
-  font-style: normal;
-}
-.box {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 500px;
-  .userboard {
-    align-items: center;
-    //
-    display: flex;
-    justify-content: space-around;
-    margin-top: 165px;
-    width: 1390px;
-    height: 228px;
-    border-radius: 25px;
-    border: #000 solid 1px;
-    .userLeft {
-      display: flex;
-      .userImg {
-        width: 14.5%;
-        margin-left: 45px;
-      }
-      .user {
-        font-family: 'BMJUA';
-        font-size: 1.5em;
-        letter-spacing: 1px;
-        margin-left: 32px;
-        margin-top: 14px;
-        .smalluser {
-          font-size: 0.7em;
-          letter-spacing: 1px;
-          .ip {
-            margin-left: 5px;
-            color: #ff6666;
-          }
-          .rank {
-            margin-left: 5px;
-          }
-        }
-      }
-    }
-    .userright {
-      display: flex;
-      gap: 150px;
-      margin-left: 150px;
-    }
-    .right {
-      align-items: center;
-      text-align: center;
-      font-family: 'BMJUA';
-      font-size: 1.2em;
-      margin-top: 20px;
-      margin-left: -170px;
-      width: 250px;
-      .num {
-        margin-top: 10px;
-        font-size: 1.3em;
-      }
-    }
-  }
-}
-.bottom {
-  display: flex;
-  justify-content: center;
-}
-</style>
+<style lang="scss" scoped></style>
