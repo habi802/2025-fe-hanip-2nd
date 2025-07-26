fba12154d420e83f6a5113e895843fbd5bd77c66
munsohyeon <msh1787@gmail.com>
2025-07-27 02:23:19 +0900

    장바구니 로그인전 상태 제외 완료

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -82,6 +82,16 @@ const groupedItems = computed(() => {
   }
   return Object.values(groups);
 });
+
+// 총가격
+const grandTotalPrice = computed(() => {
+  return groupedItems.value.reduce((groupSum, group) => {
+    return (
+      groupSum +
+      group.items.reduce((sum, item) => sum + item.price * item.quantity, 0)
+    );
+  }, 0);
+});
 </script>
 
 <template>
@@ -116,14 +126,76 @@ const groupedItems = computed(() => {
   </div>
 
   <!-- 1. 비로그인 상태 -->
-  <div v-if="!loggedIn" class="div18">
-    <p class="login-cart">로그인을 하시면 장바구니를 사용할 수 있습니다.</p>
+  <div v-if="!loggedIn" class="unlogin">
+    <div class="store-layout">
+      <div class="store-card">
+        <img class="thumbnail" src="@/imgs/chicken.png" alt="도리신 닭도리탕" />
+        <div class="store-content">
+          <h3 class="store-name">도리신 닭도리탕</h3>
+
+          <div class="store-meta">
+            <div class="rating">
+              <img id="icon" src="/src/imgs/star.png" alt="별점" />
+              <span class="score">4.8</span>
+              <span class="count">(983)</span>
+            </div>
+            <div class="likes">
+              <img id="icon" src="/src/imgs/love.png" alt="찜" />
+              <span class="like-count">927</span>
+            </div>
+          </div>
+
+          <div class="store-info">
+            <p>최소 주문 금액 10,000원</p>
+            <p>배달료 0원 ~ 3,000원</p>
+          </div>
+        </div>
+      </div>
+      <div
+        v-for="group in groupedItems"
+        :key="group.storeName"
+        class="store-box"
+      >
+        <!-- 가게 음식 정보 -->
+        <div class="store-info">
+          <p class="store-name">{{ group.storeName }}</p>
+          <p class="store-sub">{{ group.storeNotice }}</p>
+        </div>
+        <!-- 장바구니 음식 리스트 -->
+        <div v-for="item in group.items" :key="item.id" class="cart-item">
+          <img
+            :src="item.image_path"
+            alt="음식 이미지"
+            style="width: 60px; height: 60px"
+          />
+          <div class="item-content">
+            <p class="item-name">{{ item.name }}</p>
+            <p class="item-comment"></p>
+            <div class="qty-box">
+              <button @click="decreaseQty(item)">-</button>
+              <span>{{ item.quantity }}</span>
+              <button @click="increaseQty(item)">+</button>
+            </div>
+            <p class="item-price">
+              {{ (item.price * item.quantity).toLocaleString() }}원
+            </p>
+            <!-- <button @click="remove(item.id)">X</button> -->
+          </div>
+        </div>
+        <!-- 각 그룹마다 주문 버튼 배치 -->
+        <div class="cart-footer">
+          <p class="total">총 결제 금액:</p>
+          <p class="total">{{ grandTotalPrice.toLocaleString() }}원</p>
+        </div>
+      </div>
+    </div>
+    <!-- 주문하기 버튼 -->
     <div class="groupContainer">
-      <button class="div-login" @click="goToLogin">로그인하기</button>
+      <div class="div19" @click="goToOrder(group)">로그인후 주문하기</div>
     </div>
   </div>
 
-  <!-- 2. 로그인했지만 장바구니 비어있음 -->
+  <!-- 1. 빈 장바구니 -->
   <div v-else-if="state.items.length === 0">
     <div class="div18">장바구니에 담은 음식이 없습니다.</div>
     <div class="groupContainer">
@@ -134,48 +206,74 @@ const groupedItems = computed(() => {
     </div>
   </div>
 
-  <!-- 3. 장바구니에 음식 있음 -->
+  <!-- 2. 장바구니에 음식 있음 -->
   <div v-else>
-    <div v-for="group in groupedItems" :key="group.storeName" class="store-box">
-      <div class="store-info">
-        <p class="store-name">{{ group.storeName }}</p>
-        <p class="store-sub">{{ group.storeNotice }}</p>
-      </div>
+    <!-- 음식점 가게 카드 -->
+    <div class="store-layout">
+      <div class="store-card">
+        <img class="thumbnail" src="@/imgs/chicken.png" alt="도리신 닭도리탕" />
+        <div class="store-content">
+          <h3 class="store-name">도리신 닭도리탕</h3>
 
-      <div v-for="item in group.items" :key="item.id" class="cart-item">
-        <img
-          :src="item.image_path"
-          alt="음식 이미지"
-          style="width: 60px; height: 60px"
-        />
-        <div class="item-content">
-          <p class="item-name">{{ item.name }}</p>
-          <div class="qty-box">
-            <button @click="decreaseQty(item)">-</button>
-            <span>{{ item.quantity }}</span>
-            <button @click="increaseQty(item)">+</button>
+          <div class="store-meta">
+            <div class="rating">
+              <img id="icon" src="/src/imgs/star.png" alt="별점" />
+              <span class="score">4.8</span>
+              <span class="count">(983)</span>
+            </div>
+            <div class="likes">
+              <img id="icon" src="/src/imgs/love.png" alt="찜" />
+              <span class="like-count">927</span>
+            </div>
           </div>
-          <p class="item-price">
-            {{ (item.price * item.quantity).toLocaleString() }}원
-          </p>
-          <button @click="remove(item.id)">X</button>
-        </div>
-      </div>
 
-      <!-- 각 그룹마다 주문 버튼 배치 -->
-      <div class="cart-footer">
-        <p class="total">
-          총 금액:
-          {{
-            group.items
-              .reduce((sum, item) => sum + item.price * item.quantity, 0)
-              .toLocaleString()
-          }}원
-        </p>
-        <div class="groupContainer">
-          <div class="div19" @click="goToOrder(group)">주문하기</div>
+          <div class="store-info">
+            <p>최소 주문 금액 10,000원</p>
+            <p>배달료 0원 ~ 3,000원</p>
+          </div>
         </div>
       </div>
+      <div
+        v-for="group in groupedItems"
+        :key="group.storeName"
+        class="store-box"
+      >
+        <!-- 가게 음식 정보 -->
+        <div class="store-info">
+          <p class="store-name">{{ group.storeName }}</p>
+          <p class="store-sub">{{ group.storeNotice }}</p>
+        </div>
+        <!-- 장바구니 음식 리스트 -->
+        <div v-for="item in group.items" :key="item.id" class="cart-item">
+          <img
+            :src="item.image_path"
+            alt="음식 이미지"
+            style="width: 60px; height: 60px"
+          />
+          <div class="item-content">
+            <p class="item-name">{{ item.name }}</p>
+            <p class="item-comment"></p>
+            <div class="qty-box">
+              <button @click="decreaseQty(item)">-</button>
+              <span>{{ item.quantity }}</span>
+              <button @click="increaseQty(item)">+</button>
+            </div>
+            <p class="item-price">
+              {{ (item.price * item.quantity).toLocaleString() }}원
+            </p>
+            <!-- <button @click="remove(item.id)">X</button> -->
+          </div>
+        </div>
+        <!-- 각 그룹마다 주문 버튼 배치 -->
+        <div class="cart-footer">
+          <p class="total">총 결제 금액:</p>
+          <p class="total">{{ grandTotalPrice.toLocaleString() }}원</p>
+        </div>
+      </div>
+    </div>
+    <!-- 주문하기 버튼 -->
+    <div class="groupContainer">
+      <div class="div19" @click="goToOrder(group)">주문하기</div>
     </div>
   </div>
 </template>
@@ -189,59 +287,70 @@ const groupedItems = computed(() => {
   font-weight: normal;
   font-style: normal;
 }
-
-* {
-  font-family: "BMJUA";
+@font-face {
+  // 프리텐다드
+  font-family: "Pretendard-Regular";
+  src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+    format("woff");
+  font-weight: 400;
+  font-style: normal;
 }
+
 .cart-empty-wrapper {
+  // 메인 헤더부분(?)
   max-width: 1497px;
-  margin: 50px auto;
+  margin: 50px auto 0 auto;
   display: flex;
   flex-direction: column;
   margin-top: 80px;
 }
 
-.title-wrap { // < 장바구니
+.title-wrap {
+  // < 장바구니
+  font-family: "BMJUA";
   display: flex;
   align-items: center;
 
-  .back-icon { // <
+  .back-icon {
+    // <
     width: 50px;
     height: 50px;
     margin-right: 10px;
   }
-  
-  .div29 { // 장바구니
+
+  .div29 {
+    // 장바구니
     font-size: 50px;
+  }
+}
+.step-horizontal {
+  // 01 음식선택 > 02 장바구니 > 03 주문/결제 > 04 주문 완료
+  font-family: "BMJUA";
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 20px;
+
+  .step-text.current {
+    // 02 장바구니
+    color: #ff6666;
   }
 }
 
 .top-row {
+  // 글씨 박스
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 80px;
+  margin-bottom: 30px;
 }
 
 .header-row {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  gap: 20px;
+  gap: 0px;
 }
-
-
-.step-horizontal { // 01 음식선택 > 02 장바구니 > 03 주문/결제 > 04 주문 완료
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  font-size: 20px;
-  
-  .step-text.current { // 02 장바구니
-    color: #ff6666;
-  }
-}
-
 
 .arrow img {
   width: 12px;
@@ -255,6 +364,7 @@ const groupedItems = computed(() => {
   color: #555;
 }
 
+// 빈 장바구니
 .div18 {
   // 장바구니에 담은 음식이 없습니다.
   font-size: 32px;
@@ -294,7 +404,8 @@ const groupedItems = computed(() => {
   margin-bottom: 40px;
 }
 
-.div19 { // 음식 담으러 가기 버튼
+.div19 {
+  // 음식 담으러 가기 버튼
   width: 400px;
   height: 80px;
   border: 2px solid #ff6666;
@@ -307,29 +418,126 @@ const groupedItems = computed(() => {
   cursor: pointer;
 
   // 글자 수직 정렬
-  display: flex; 
-  justify-content: center; 
-  align-items: center; 
-  text-align: center; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 
   &:hover {
     background-color: #ffe5e5;
   }
 }
 
-// 중앙 카드 형태의 store-box 스타일 
+// 박스 정렬
+.store-layout {
+  max-width: 1251px;
+  margin: 40px auto 0 auto; // 전체 가운데 정렬
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 40px;
+  flex-wrap: nowrap; // 줄바꿈 방지
+}
+
+// 음식점 가게 카드
+.store-card {
+  width: 368px; // 카드 전체 너비
+  border: 1px solid #d7d7d7;
+  border-radius: 25px;
+  overflow: hidden; // 내부 요소 넘칠 경우 숨김
+  cursor: pointer;
+
+  .thumbnail {
+    // 사진
+    width: 370px; // 썸네일 가로 꽉 차게
+    height: 325px; // 썸네일 고정 높이
+    object-fit: cover; // 이미지 비율 유지하며 채우기
+  }
+
+  .store-content {
+    padding: 20px; // 내부 여백
+
+    .store-name {
+      font-family: "BMJUA";
+      font-size: 25px;
+      font-weight: bold;
+      margin-bottom: 12px;
+      color: #000;
+      text-align: center;
+    }
+
+    .store-meta {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+      margin-bottom: 12px;
+      margin: 20px 25%;
+
+      .rating,
+      .likes {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+      }
+
+      img {
+        // 별, 하트 아이콘
+        width: 20px;
+        height: 20px;
+      }
+
+      .score {
+        font-weight: bold; // 별점 숫자 굵게
+        color: #6c6c6c; // 짙은 회색
+      }
+
+      .count {
+        color: #797979;
+        font-size: 18px;
+        text-align: center;
+      }
+    }
+    .store-info {
+      color: #797979;
+      font-size: 18px;
+      margin-bottom: 20px;
+      text-align: center;
+    }
+  }
+}
+
 .store-box {
-  max-width: 900px;
-  margin: 0 auto 40px auto;
-  padding: 24px;
+  // 각 음식점별 장바구니 박스
+  width: 830px;
+  height: 553px;
+  margin: 0 auto 0px 50px;
+  padding: 50px;
   border: 1px solid #e0e0e0;
   border-radius: 16px;
   background-color: #fff;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.06);
-}
+  display: flex;
+  flex-direction: column; // 세로 정렬
 
-.store-info {
-  margin-bottom: 20px;
+  .store-info {
+    // 음식점 정보 영역 (이름, 공지사항)
+    margin-bottom: 20px;
+  }
+
+  .cart-footer {
+    // 총금액 구역박스
+    margin-top: auto;
+    display: flex;
+    justify-content: flex-end;
+    gap: 20px;
+    text-align: right;
+
+    .total {
+      // 총 금액
+      font-family: "BMJUA";
+      font-size: 30px;
+      margin-right: 40px;
+    }
+  }
 }
 
 .store-name {
@@ -343,75 +551,96 @@ const groupedItems = computed(() => {
 }
 
 .cart-item {
+  // 메뉴
   display: flex;
   align-items: center;
   margin-bottom: 16px;
   gap: 16px;
-}
 
-.cart-item img {
-  width: 64px;
-  height: 64px;
-  border-radius: 12px;
-  object-fit: cover;
+  img {
+    // 장바구니 이미지
+    width: 100px;
+    height: 100px;
+    border-radius: 15px;
+    object-fit: cover;
+  }
 }
 
 .item-content {
+  // 메뉴 박스
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 20px;
   flex: 1;
-}
 
-.item-name {
-  font-weight: 600;
-  flex: 1;
+  .item-name {
+    // 메뉴 이름
+    font-family: "BMJUA";
+    font-size: 20px;
+    font-weight: 600;
+    flex: 1;
+    margin-bottom: 8px;
+    margin-left: 24px;
+  }
+
+  .item-comment {
+    // 메뉴 설명글
+    font-family: "Pretendard-Regular";
+    color-scheme: #a9a9a9;
+    font-size: 15px;
+  }
+  .item-price {
+    // 메뉴 가격
+    font-family: "BMJUA";
+    font-size: 20px;
+    margin-bottom: -0.5px;
+    margin-left: 70px;
+  }
+
+  button {
+    font-weight: bold;
+    cursor: pointer;
+  }
 }
 
 .qty-box {
+  // 수량 조절 박스
+  font-family: "Pretendard-Regular";
+  font-size: 20px;
   display: flex;
   align-items: center;
-  border: 1px solid #ccc;
-  border-radius: 6px;
-  overflow: hidden;
-}
+  gap: 10px;
+  text-align: center;
 
-.qty-box button {
-  padding: 4px 10px;
-  background-color: #fff;
-  border: none;
-  font-size: 14px;
-  cursor: pointer;
-}
+  button {
+    // 수량버튼
+    width: 25px;
+    height: 25px;
 
-.qty-box span {
-  padding: 0 12px;
-}
+    // 테두리 및 배경 설정
+    border: 1px solid #999;
+    border-radius: 6px;
+    background-color: #fff;
+    color: #000;
 
-.item-price {
-  font-weight: bold;
-  margin-left: 16px;
-  min-width: 80px;
-  text-align: right;
-}
+    // 가운데 정렬 및 클릭 커서 설정
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-.item-content button {
-  margin-left: 16px;
-  background: none;
-  border: none;
-  color: #ff6666;
-  font-weight: bold;
-  cursor: pointer;
-}
+  span {
+    // 숫자 표시 박스 크기 설정
+    width: 32px;
+    height: 32px;
 
-.cart-footer {
-  margin-top: 40px;
-  text-align: right;
-}
-
-.total {
-  font-size: 18px;
-  font-weight: bold;
-  margin-bottom: 20px;
+    // 텍스트 정렬 설정
+    text-align: center;
+    line-height: 32px;
+    font-size: 16px;
+    font-weight: bold;
+  }
 }
 </style>
