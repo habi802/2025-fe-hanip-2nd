f452b45ac6bc104306e56779945f3ee79139af69
munsohyeon <msh1787@gmail.com>
2025-09-12 11:11:00 +0900

    찜하기 수정 완료

--- src/views/customer/Contact.vue
+++ src/views/customer/Contact.vue
@@ -9,23 +9,23 @@ import ContactMain from "@/components/customerService/ContactMain.vue";
         <h2 class="title">FAQ</h2>
         <div class="titleLine"></div>
       </div>
-        <ContactMain />
+      <ContactMain />
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-    .title {
-        font-size:40px;
-        font-weight: 600;
-        text-align: center;
-        margin-top: 80px;
-    }
-        // 메인 선
-        .titleLine {
-            width: 1480px;
-      border-bottom: 2px solid #000;
-      margin-top: 80px;
-      margin-bottom: 30px;
-    }
+.title {
+  font-size: 40px;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 80px;
+}
+// 메인 선
+.titleLine {
+  width: 1480px;
+  border-bottom: 2px solid #000;
+  margin-top: 80px;
+  margin-bottom: 30px;
+}
 </style>
