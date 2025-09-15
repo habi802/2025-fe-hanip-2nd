51711eb5ff7553b07fd6d6d3dd5dfa5c9d07ee05
100.0.6.biz <100.0.6.biz@gamil.com>
2025-09-15 17:56:43 +0900

    사장 가게상태관리 퍼블리싱중

--- src/views/owner/StatusStore.vue
+++ src/views/owner/StatusStore.vue
@@ -1,101 +1,51 @@
 <script setup>
 import { ref } from 'vue';
+import StoreStatusStaticInfo from '@/components/owner/StoreStatusStaticInfo.vue';
+import StoreStatusControl from '@/components/owner/StoreStatusControl.vue';
 
-const selectedLang = ref('02'); // 기본값 02
-
-const store = {
-  ownerName : "홍길동",
-  adds:"대구광역시 어디구 어디동 000길 234",
-  addsadd:"503동 1380호 센트럴머시기APT"
-
-}
-
+const selected = ref('basic'); // 기본으로 '기본정보'
 
 
 </script>
 
 <template>
-  <form class="white-card">
-    <div class="store-info-wrap1">
-      <span>대표자 이름</span>
-      <div class="gray-content">{{ store.ownerName }}</div>
-      <span>가게 상호명</span>
-      <div class="gray-content">{{ store.ownerName }}</div>
-      <span>사업자등록번호</span>
-      <div class="gray-content">{{ store.ownerName }}</div>
-      <span>가게주소</span>
-        <div class="address-box gray-content">
-          {{ store.adds + store.addsadd  }}
-        </div>
-      <span>가게전화</span>
-      <div class="d-flex">
-        <select class="gray-content" name="lang" id="label" required>
-          <option>02</option>
-          <option>053</option>
-          <option>010</option>
-          <!-- 직접입력 선택 시 보여주고 아니면 숨기기 -->
-          <option :v-bind="">직접입력<input v-if="selectedLang === '직접입력'" class="gray-content" placeholder="직접입력" /></option>
-
-        </select>
-        <input class="gray-content"></input>
-        <input class="gray-content"></input>
-      </div>
-      <span>업종</span>
-      <input class="gray-content"></input>
+  <div class="section-wrap">
+    <div class="btn-group gap-3" role="group">
+      <button type="button" class="submenu-btn"  :class="selected === 'basic' ? 'submenu-active' : 'submenu-none'"  @click="selected = 'basic'">
+        기본정보
+      </button>
+      <button  type="button"  class="submenu-btn"  :class="selected === 'control' ? 'submenu-active' : 'submenu-none'" @click="selected = 'control'">
+        운영관리
+      </button>
     </div>
-
-    <div class="store-info-wrap2">
-        <span>
-          가게 대표 이미지
-        </span>
-        <button>이미지를 등록하세요</button>
-        <input id="imgInput" type="file" accept="image/*" style="display:none" />
-        <img id="imgPreview" src="" alt="미리보기 이미지" style="max-width: 300px; margin-top: 10px; display:none;" />
-        <span>
-          가게 소개글
-        </span>
-        <textarea placeholder="최대 200자 이하 짧은 홍보문구를 작성해보세요!">
-        </textarea>
-      </div>
-  </form>
-
+    <div class="white-card">
+      <StoreStatusStaticInfo v-if="selected === 'basic'" />
+      <StoreStatusControl v-if="selected === 'control'" />
+    </div>
+  </div>
 </template>
 
 
 <style scoped lang="scss">
-.white-card{
-  display: flex;
-  width: 95%;
-  height: 90%;
-  align-items: center;
-  justify-content: center;
-  gap: 50px;
-}
-.store-info-wrap1{
-  display: grid;
-  grid-template-columns: 100px 620px;
-  align-items: center;
-  gap: 20px;
-}
-
-.store-info-wrap2{
+.section-wrap{
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  .address-box{
-    display: flex;
-    flex-direction: column;
-  }
-  button{
-    width: 420px;
-    height: 230px;
-    border-radius: var(--card-lg-radius);
-    border: none;
-  }
-  textarea{
-    border-radius: var(--card-lg-radius);
-    border: 1px solid var(--grey2);
-  }
+  justify-content: center;
+  align-items: center;
+}
+.white-card{
+  width: 95%;
+  height: 95%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn-group {
+  align-self: flex-start;
+  margin-left: 5%; /* white-card가 width 95%니까 같은 위치에 맞추려면 */
 }
 
 </style>
