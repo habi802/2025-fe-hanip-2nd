35fd33a7d52c7141e3da8abbedbde57fc808daa5
djinylh <d.jinylh@gmail.com>
2025-09-18 16:23:03 +0900

    리뷰 모달 완료

--- src/components/customer/OrderTest.vue
+++ src/components/customer/OrderTest.vue
@@ -1,11 +1,19 @@
 <script setup>
+import ReviewModal from '../modal/ReviewModal.vue';
+import { ref } from 'vue';
 
+const reviewModal = ref(null);
+
+const openModal = () => {
+    const modalElement = reviewModal.value.$el;
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+};
 </script>
 
 <template>
-asfasf
+    <div class="btn" @click="openModal">asfasfasfafs </div>
+    <ReviewModal ref="reviewModal"></ReviewModal>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
