f4a591899d4a101e39c832d7c9b45d081b51e618
manew10006 <100.0.6.biz@gmail.com>
2025-09-24 17:05:09 +0900

    통계화면 세팅수정

--- src/views/owner/Donations.vue
+++ src/views/owner/Donations.vue
@@ -3,11 +3,20 @@
 </script>
 
 <template>
-  <div class="white-card">
-    통계화면 준비중
+  
+  <div class="wrap">
+    <div class="white-card">
+      통계화면 준비중
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.wrap{
+  width: 100px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
